c9f97339182eec46caa04c5324a90f520a449326
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-27 18:23:19 +0300

    new version

--- app/src/main/java/com/example/melentyev/sergey/memories/CrimeActivity.java
+++ app/src/main/java/com/example/melentyev/sergey/memories/CrimeActivity.java
@@ -1,22 +1,11 @@
 package com.example.melentyev.sergey.memories;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            manager.beginTransaction().add(R.id.fragment_container, fragment, null).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
---
+++ app/src/main/java/com/example/melentyev/sergey/memories/SingleFragmentActivity.java
@@ -0,0 +1,24 @@
+package com.example.melentyev.sergey.memories;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment, null).commit();
+        }
+    }
+}
