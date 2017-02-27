a4419a08a384c5795b216c0ca26fed5a887bf176
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-27 14:19:44 +0300

    new version

---
+++ app/src/main/java/com/example/melentyev/sergey/memories/CrimeFragment.java
@@ -0,0 +1,47 @@
+package com.example.melentyev.sergey.memories;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+        mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // logic
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTitleField.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // logic
+            }
+        });
+
+        return view;
+    }
+
+}
